fa1d1d85488f954f648d87883f99685dabb4faae
DevilsTouch <bogdanbarna565@gmail.com>
2023-02-19 00:36:20 +0200

    Server handles multiple netcat commands

--- listener.c
+++ listener.c
@@ -6,31 +6,36 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h> // read(), write(), close()
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
-   
+#define max_char 80
+#define port 8080
+
+
 // Function to read the sent data
-void func(int connfd)
+void read_and_print_buff(int client_sock)
 {
-        char buff[MAX];
-        bzero(buff, MAX);
+        char buff[max_char];
+        bzero(buff, max_char);
         // read the message from client and copy it in buffer
-        read(connfd, buff, sizeof(buff));
+        read(client_sock, buff, sizeof(buff));
         // print buffer which contains the client contents
+        printf("\n");
+        printf("Client said:  ");
         printf(buff);
-        bzero(buff, MAX);
+        printf("\n");
+        bzero(buff, max_char);
+
 }
-   
+
 // Driver function
 int main()
 {
-    int sockfd, connfd, len;
+
+    int socket_desc, client_sock, client_size, listen_backlog=5;
     struct sockaddr_in servaddr, cli;
-   
+
     // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
     {
         printf("socket creation failed...\n");
         exit(0);
@@ -39,23 +44,27 @@ int main()
         printf("Socket successfully created..\n");
 
     bzero(&servaddr, sizeof(servaddr));
-   
+
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
-   
+    servaddr.sin_port = htons(port);
+
     // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0)
+    if ((bind(socket_desc, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0)
     {
         printf("socket bind failed...\n");
         exit(0);
     }
     else
         printf("Socket successfully binded..\n");
-   
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0)
+
+
+//Starts listening for packets and printing them
+while(1)
+{
+     // Now server is ready to listen and verification
+    if ((listen(socket_desc, listen_backlog)) != 0)
     {
         printf("Listen failed...\n");
         exit(0);
@@ -63,21 +72,22 @@ int main()
     else
         printf("Server listening..\n");
 
-    len = sizeof(cli);
-   
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0)
+//Store the client’s address and socket descriptor by accepting an incoming connection
+    client_size = sizeof(cli);
+    client_sock = accept(socket_desc, (struct sockaddr*)&cli, &client_size);
+    if (client_sock < 0)
     {
         printf("server accept failed...\n");
         exit(0);
     }
     else
         printf("server accept the client...\n");
-   
-    // Function for chatting between client and server
-    func(connfd);
-   
-    // After chatting close the socket
-    close(sockfd);
+
+//Print data of client to termnal
+    read_and_print_buff(client_sock);
+    close(client_sock);
+    printf("Closed client socket...\n");
+}
+
+
 }
